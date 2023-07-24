b4ed8a94ef5b7dd0fdcdf0956ba7cb8db45d6e07
Andrew Domingues <drewdomi@proton.me>
2023-07-24 12:58:54 -0400

    feat: add findbyParams

--- src/controllers/customers.ts
+++ src/controllers/customers.ts
@@ -6,7 +6,9 @@ const prisma = new PrismaClient();
 export const customerController = {
   async getCustomers(req: Request, res: Response) {
     try {
+
       const allCustomers = await prisma.customers.findMany();
+
       res.send(allCustomers);
     } catch (error) {
       res.send(error);
@@ -14,18 +16,18 @@ export const customerController = {
   },
   async createCustomer(req: Request, res: Response) {
     try {
-      const { cpf } = req.body
 
+      const { cpf } = req.body
       const customer = await prisma.customers.findUnique({
         where: {
           cpf: cpf
         }
       })
-      if (customer) res.json({ error: "Cliente já cadastrado!!" })
+      if (customer) throw { error: "Cliente já cadastrado!!" }
 
       const createdCustomer = await prisma.customers.create({ data: req.body });
-      res.json(createdCustomer);
 
+      res.send(createdCustomer);
     } catch (error) {
       res.send(error);
     }
@@ -34,15 +36,33 @@ export const customerController = {
   async findById(req: Request, res: Response) {
     const { id } = req.params
     try {
-      console.log(req.params)
+
       const customer = await prisma.customers.findUnique({
         where: {
           id: Number(id)
         }
       })
+      if (!customer) throw { error: "Cliente não encontrado" }
 
-      if (!customer) res.json({ error: "Cliente não encontrado" })
-      res.json(customer);
+      res.send(customer);
+    } catch (error) {
+      res.send(error);
+    }
+  },
+
+  async findByParams(req: Request, res: Response) {
+    const { name, cpf } = req.query
+    try {
+
+      const customer = await prisma.customers.findMany({
+        where: {
+          name: {
+            contains: name?.toString()
+          }
+        }
+      })
+
+      res.send(customer);
     } catch (error) {
       res.send(error);
     }
