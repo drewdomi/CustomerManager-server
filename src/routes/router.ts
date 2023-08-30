b0e66fa19f4989520ca8a15fa9b26bd434bae5f9
Andrew Domingues <drewdomi@proton.me>
2023-08-30 17:17:36 -0300

    feat: add endpoint to verify-cpf

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { customerController } from "../controllers/customers";
+import { cpfController } from "../controllers/cpf";
 
 const router = Router();
 
@@ -9,5 +10,6 @@ router.get("/customers/find", customerController.findByParams);
 router.get("/customers/:id", customerController.findById);
 router.put("/customers/:id", customerController.editCustomer);
 router.get("/", customerController.testApi);
+router.get("/verify-cpf/:cpf", cpfController.verifyCpf);
 
 export default router;
